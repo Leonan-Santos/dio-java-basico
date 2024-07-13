564d0f112795e4921d4b68bcd975d533c6c445c8
Leonan Santos <leonam1997@outlook.com>
2024-07-13 20:28:32 -0300

    Seguindo curso

--- trilha-java-basico/operadores/operadores/src/Operadores.java
+++ trilha-java-basico/operadores/operadores/src/Operadores.java
@@ -1,5 +1,11 @@
 public class Operadores {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2)
+        
+        System.out.println("");
     }
 }
